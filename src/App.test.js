58e07f312ce1d48f520836506b275ea60b47b8be
Maíra Martins K <mairamartinsk@gmail.com>
2018-11-29 20:35:26 +0100

    test: check that App is loading child components

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { shallow } from "enzyme";
 import App from "./App";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("App Component", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it("loads the Gallery component", () => {
+    const wrapper = shallow(<App />);
+    const gallery = wrapper.find("Gallery");
+    expect(gallery.length).toEqual(1);
+  });
 });
