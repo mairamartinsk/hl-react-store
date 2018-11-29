c862520057c237f5f32f4fb08b212bc1065ba5d3
Maíra Martins K <mairamartinsk@gmail.com>
2018-11-29 20:31:14 +0100

    feat: Implement grid layout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Header extends Component {
   render() {
     return (
-      <header className="Header">
+      <header className="header">
         <p>BlackWood</p>
       </header>
     );
