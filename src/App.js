0e7656f3392f3acff0254f0fc722fdb2d69419d3
Maíra Martins K <mairamartinsk@gmail.com>
2018-11-30 01:12:18 +0100

    Implement shopping cart total

--- src/App.js
+++ src/App.js
@@ -11,27 +11,36 @@ class App extends Component {
   state = {
     imagesToDisplay: 6,
     imagePlaceholder: "https://source.unsplash.com/collection/3360193/400x400",
-    displayModal: false,
-    cartItems: 0
+    isModal: false,
+    cartItems: 0,
+    cartTotal: 0
   };
 
   showModal = () => {
-    this.setState({ displayModal: true });
+    this.setState({ isModal: true });
   };
 
   hideModal = () => {
-    this.setState({ displayModal: false });
+    this.setState({ isModal: false });
   };
 
   addToCart = () => {
     this.setState(state => {
       return { cartItems: state.cartItems + 1 };
     });
+    this.updateCartTotal();
   };
 
   removeFromCart = () => {
     this.setState(state => {
       return { cartItems: state.cartItems - 1 };
+    });
+    this.updateCartTotal();
+  };
+
+  updateCartTotal = () => {
+    this.setState(state => {
+      return { cartTotal: state.cartItems * 25 };
     });
   };
 
@@ -39,19 +48,18 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-        {this.state.displayModal && (
-          <Modal
-            hideModal={this.hideModal}
-            cartItems={this.state.cartItems}
-            addToCart={this.addToCart}
-            removeFromCart={this.removeFromCart}
-          />
-        )}
+        <Modal
+          isModal={this.state.isModal}
+          hideModal={this.hideModal}
+          cartItems={this.state.cartItems}
+          addToCart={this.addToCart}
+          removeFromCart={this.removeFromCart}
+        />
         <Gallery
           image={this.state.imagePlaceholder}
           items={this.state.imagesToDisplay}
           cartItems={this.state.cartItems}
-          addToCart={this.addToCart}
+          addToCart={this.addToCart.bind(this)}
         />
         <Aside showModal={this.showModal} cartItems={this.state.cartItems} />
       </div>
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Modal extends Component {
   render() {
-    const classes = this.props.showModal
+    const classes = this.props.isModal
       ? "modal display-block"
       : "modal display-none";
 
@@ -10,6 +10,7 @@ class Modal extends Component {
       <div className={classes}>
         <button onClick={this.props.hideModal}>close</button>
         <p>Modal!!!</p>
+        <p>Items on cart: {this.props.cartItems}</p>
         <button onClick={this.props.addToCart}>add to cart</button>
         <button onClick={this.props.removeFromCart}>remove from cart</button>
       </div>
